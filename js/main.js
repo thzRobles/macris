e5aa4247e9c8380c18168b5b6ca9b8e5425b9c92
thzro <thz.robles@gmail.com>
2023-07-16 20:48:22 -0700

    Se modifico pagina

--- js/main.js
+++ js/main.js
@@ -10,3 +10,32 @@ document.addEventListener('DOMContentLoaded', () => {
         noWrap: false,
     });
 });
+
+//javascript for responsive navigation sidebar menu
+window.addEventListener("scroll", function() {
+    // var header = document.querySelector("header")
+    var header = document.querySelector("header")
+    header.classList.toggle("down", window.scrollY > 0);
+
+    // //change logo
+    // var logo = document.querySelector(".logo img");
+	// if (window.scrollY>0) 
+    // {
+	//     logo.setAttribute('src', 'img/logo/newlogo/newlogoblack.png');
+	// }else {
+	//     logo.setAttribute('src', 'img/logo/newlogo/newlogowhite.png');
+	// }
+
+});
+
+// show effect
+let ubication = window.pageYOffset;
+window.onscroll = function() {
+    let displacement = window.pageYOffset;
+    if (ubication >= displacement) {
+        document.getElementById('section-header').style.top = '0';
+    }else {
+        document.getElementById('section-header').style.top = '-100px'
+    }
+    ubication = displacement;
+}
